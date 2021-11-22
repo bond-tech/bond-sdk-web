d37dc9bbd0c023042cf57810989f4b3aa6405b5b
osylko <oleksandr@bond.tech>
2021-11-22 15:55:14 +0200

    comment skipped tests

--- cypress/integration/1.sample-card-show.spec.js
+++ cypress/integration/1.sample-card-show.spec.js
@@ -12,35 +12,35 @@ describe('Sample Card Show Form', () => {
     cy.getTempToken();
   });
 
-  it.skip('fill the form out in sample card show (Credit)', () => {
-    // find card id, and temporary token inputs
-    // and fill the form out - credit catd id
-    cy.fillBrandBackendSectionOut(Cypress.env('CREDIT_CARD_ID'));
-  });
+  // it.skip('fill the form out in sample card show (Credit)', () => {
+  //   // find card id, and temporary token inputs
+  //   // and fill the form out - credit catd id
+  //   cy.fillBrandBackendSectionOut(Cypress.env('CREDIT_CARD_ID'));
+  // });
 
-  it.skip('reveal card information in sample card show (Credit)', () => {
-    // clean up default values in card number, expiration date and cvv
-    cy.get('#num').invoke('val', '');
-    cy.get('#exp').invoke('val', '');
-    cy.get('#cvv').invoke('val', '');
-
-    // fill out the form (credit card id, temporary token) in brand backend section
-    cy.fillBrandBackendSectionOut(Cypress.env('CREDIT_CARD_ID'));
-
-    cy.intercept('GET', `/api/v0/cards/${Cypress.env('CREDIT_CARD_ID')}`).as(
-      'credit_card_show'
-    );
-
-    cy.get('#toggle').click();
-    cy.wait('@credit_card_show');
-
-    // varify the response, the returned credit card number, expiration date and cvv
-    cy.verifyReavealedInfo(
-      Cypress.env('CREDIT_CARD_NUMBER'),
-      Cypress.env('CREDIT_CARD_EXP'),
-      Cypress.env('CREDIT_CARD_CVV')
-    );
-  });
+  // it.skip('reveal card information in sample card show (Credit)', () => {
+  //   // clean up default values in card number, expiration date and cvv
+  //   cy.get('#num').invoke('val', '');
+  //   cy.get('#exp').invoke('val', '');
+  //   cy.get('#cvv').invoke('val', '');
+  //
+  //   // fill out the form (credit card id, temporary token) in brand backend section
+  //   cy.fillBrandBackendSectionOut(Cypress.env('CREDIT_CARD_ID'));
+  //
+  //   cy.intercept('GET', `/api/v0/cards/${Cypress.env('CREDIT_CARD_ID')}`).as(
+  //     'credit_card_show'
+  //   );
+  //
+  //   cy.get('#toggle').click();
+  //   cy.wait('@credit_card_show');
+  //
+  //   // varify the response, the returned credit card number, expiration date and cvv
+  //   cy.verifyReavealedInfo(
+  //     Cypress.env('CREDIT_CARD_NUMBER'),
+  //     Cypress.env('CREDIT_CARD_EXP'),
+  //     Cypress.env('CREDIT_CARD_CVV')
+  //   );
+  // });
 
   it('fill the form out in sample card show (Debit)', () => {
     // find card id, and temporary token inputs
--- cypress/integration/3.sample-pin-setting.spec.js
+++ cypress/integration/3.sample-pin-setting.spec.js
@@ -19,45 +19,45 @@ describe("Sample PIN Setting", () => {
 
   });
 
-  it.skip("fill the brand backend section out in sample pin setting (Credit)", () => {
+  // it.skip("fill the brand backend section out in sample pin setting (Credit)", () => {
+  //
+  //   // find card id, and temporary token inputs
+  //   // and fill the form out
+  //   cy.fillBrandBackendSectionOut(Cypress.env("CREDIT_CARD_ID"));
+  //
+  // });
 
-    // find card id, and temporary token inputs
-    // and fill the form out
-    cy.fillBrandBackendSectionOut(Cypress.env("CREDIT_CARD_ID"));
-
-  });
-
-  it.skip("reset PIN in sample pin setting (Credit)", () => {
-
-    // input credit card id, temporary token in brand backend section
-    cy.fillBrandBackendSectionOut(Cypress.env("CREDIT_CARD_ID"));
-
-    // input new PIN, and confirm new PIN in Brand APP section
-    cy.get("#cc-form").get("iframe").then($iframe => {
-      const $body = $iframe.contents().find("body");
-      cy.wrap($body).then(items => {
-        cy.wrap(items[1])
-          .find("[type=password]")
-          .type(Cypress.env("CREDIT_CARD_NEW_PIN"));
-        cy.wrap(items[2])
-          .find("[type=password]")
-          .type(Cypress.env("CREDIT_CARD_NEW_PIN"));
-      });
-    });
-
-    // click on submit button to send the request to reset pin
-    cy.get("[class=\"panel panel2\"]")
-      .get("[class=\"action\"]")
-      .find("#submit").click();
-    cy.wait('@pin_setting');
-
-    // verify the response indicating pin is changed successfully
-    cy.get("[class=\"panel panel2\"]")
-      .get("#result")
-      .should("have.text",
-              `card_id @ service: ${Cypress.env("CREDIT_CARD_ID")}\n      pin_changed @ service: true`);
-
-  });
+  // it.skip("reset PIN in sample pin setting (Credit)", () => {
+  //
+  //   // input credit card id, temporary token in brand backend section
+  //   cy.fillBrandBackendSectionOut(Cypress.env("CREDIT_CARD_ID"));
+  //
+  //   // input new PIN, and confirm new PIN in Brand APP section
+  //   cy.get("#cc-form").get("iframe").then($iframe => {
+  //     const $body = $iframe.contents().find("body");
+  //     cy.wrap($body).then(items => {
+  //       cy.wrap(items[1])
+  //         .find("[type=password]")
+  //         .type(Cypress.env("CREDIT_CARD_NEW_PIN"));
+  //       cy.wrap(items[2])
+  //         .find("[type=password]")
+  //         .type(Cypress.env("CREDIT_CARD_NEW_PIN"));
+  //     });
+  //   });
+  //
+  //   // click on submit button to send the request to reset pin
+  //   cy.get("[class=\"panel panel2\"]")
+  //     .get("[class=\"action\"]")
+  //     .find("#submit").click();
+  //   cy.wait('@pin_setting');
+  //
+  //   // verify the response indicating pin is changed successfully
+  //   cy.get("[class=\"panel panel2\"]")
+  //     .get("#result")
+  //     .should("have.text",
+  //             `card_id @ service: ${Cypress.env("CREDIT_CARD_ID")}\n      pin_changed @ service: true`);
+  //
+  // });
 
   it("reset PIN in sample pin setting (Debit)", () => {
 

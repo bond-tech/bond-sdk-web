854ed4e113c4931ffe25b2aaeb50ff6f593fb0eb
William Ross Morrow <ross@bond.tech>
2022-04-08 13:27:22 -0700

    options to wait-on

--- start.cypress.sh
+++ start.cypress.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-wait-on http://${CLIENT_HOST:-client}:${CLIENT_PORT:-8080}/index.html \
+# wait-on http://${CLIENT_HOST:-client}:${CLIENT_PORT:-8080}/index.html \
+wait-on ${CYPRESS_BASE_URL} \
+        --log \
+        --interval 1000 \
+        --delay 30 \
+        --timeout 300
     && npx cypress run --headless --browser chrome --record
